80e9dac3583c783170752180e327fb47b150a517
BuildTools <unconfigured@null.spigotmc.org>
2017-06-20 11:12:40 -0400

    Mail.php corrected

--- mail.php
+++ mail.php
@@ -2,11 +2,9 @@
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$telephone = $_POST['telephone'];
-	$requestPriority = $_POST['requestPriority'];
-	$emergencyType = $_POST['emergencyType'];
 	$message = $_POST['message'];
 
-	$formContent = "Form: $name \nPhone: $phone \nPriority: $requestPriority \nType: $emergencyType";
+	$formContent = "Form: $name \nPhone: $phone";
 	$reciever = "jtriantafylos@gmail.com";
 	$subject = "From my contact form";
 	$mailheader = "From: $email \r\n";
